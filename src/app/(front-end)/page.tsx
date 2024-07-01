2af6f74c0852816192fa3c98fb1b7c710f1c390b
nitin sahu <nitinsahu911111@gmail.com>
2024-07-01 18:24:27 +0530

    update>> dashboard text

--- src/app/(front-end)/page.tsx
+++ src/app/(front-end)/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="flex items-center justify-center flex-col min-h-screen">
-      <h2 className="text-4xl">Ecom</h2>
+      <h2 className="text-4xl">Ecom Craftbuy.com</h2>
     </div>
   );
 }
