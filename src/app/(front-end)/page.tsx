d104cfc7c7cd12b0d3286dd2a78002098f0ee55a
nitin sahu <nitinsahu911111@gmail.com>
2024-07-01 18:51:11 +0530

    create>> stxtx

--- src/app/(front-end)/page.tsx
+++ src/app/(front-end)/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="flex items-center justify-center flex-col min-h-screen">
-      <h2 className="text-4xl">Ecommerce cccc. Craftbuy.com cdreate dev branch
+      <h2 className="text-4xl">Ecommerce tesxt
         
       </h2>
     </div>
